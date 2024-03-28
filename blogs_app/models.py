9a25656b54fdbf26f0cd324a6ae54b1dee1daf5a
JouryABDULLAH <jours-222@outlook.com>
2024-03-27 19:59:02 -0700

    set a max length to title and category attributes in post model

--- blogs_app/models.py
+++ blogs_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class post(models.Model):
-    title = models.CharField()
-    category = models.CharField()
+    title = models.CharField(max_length = 50)
+    category = models.CharField(max_length = 15)
     content = models.TextField()
     date = models.DateTimeField()
