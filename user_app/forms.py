a8a106cb3e3052a4b2a85ff4285513d6e74751d1
JouryABDULLAH <jours-222@outlook.com>
2024-05-17 10:53:27 -0700

    create different form for login and signup

--- user_app/forms.py
+++ user_app/forms.py
@@ -1,7 +1,7 @@
 # forms.py
 
 from django import forms
-# from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 from .models import user
 
 class UserSignupForm(forms.ModelForm):
@@ -10,3 +10,7 @@ class UserSignupForm(forms.ModelForm):
     class Meta:
         model = user
         fields = ['username', 'email', 'password', 'gender']
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
--- user_app/views.py
+++ user_app/views.py
@@ -1,25 +1,35 @@
-from django.contrib import messages
-from django.shortcuts import render,redirect
-from .forms import UserSignupForm
+# views.py
 
-# Create your views here.
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from .forms import UserSignupForm, UserLoginForm
+
 def signup(request):
     if request.method == 'POST':
         form = UserSignupForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, f"Your account has been created! You are now able to log in.")
-            return redirect('/login')  # Redirect to login page after successful signup
+            messages.success(request, "Your account has been created! You are now able to log in.")
+            return redirect('login')  # Redirect to login page after successful signup
     else:
         form = UserSignupForm()
     return render(request, 'user_app/signup.html', {'form': form})
 
 def login(request):
     if request.method == 'POST':
-        form = UserSignupForm(request.POST)
+        form = UserLoginForm(data=request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('auth/login')  # Redirect to login page after successful signup
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('index')  # Redirect to the homepage or some other page after successful login
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
     else:
-        form = UserSignupForm()
+        form = UserLoginForm()
     return render(request, 'user_app/login.html', {'form': form})
